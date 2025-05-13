146d70f2502c15883111fae9f439b769b6ddb44b
Abhineet <abhineet_shrivastava@outlook.com>
2025-05-13 11:13:09 +0400

    Main Commit

---
+++ terraform/aws/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+BUCKET_NAME="mcloud-code-bucket"
+FUNCTIONS=("sentimentAnalyzer" "fetchSummary" "sendNotification")
+
+for func in "${FUNCTIONS[@]}"; do
+  cd ../../functions/$func ##
+  npm install --omit=dev
+  zip -r ../../terraform/aws/$func.zip . -x "*.test.js"
+  aws s3 cp ../../terraform//aws/$func.zip s3://$BUCKET_NAME/$func.zip
+  cd - >/dev/null 
+done
---
+++ terraform/aws/lambda.tf
@@ -0,0 +1,57 @@
+# --- Data Source: AWS Caller Identity (to get account ID) ---
+data "aws_caller_identity" "current" {}
+
+# --- Data Source: AWS Secrets Manager Secret ---
+# This assumes you have already created the secret named 'sendgrid/api_key'
+# in AWS Secrets Manager manually or via another process.
+data "aws_secretsmanager_secret" "sendgrid_api_key_secret" {
+  name = var.aws_sendgrid_secret_name # e.g., "sendgrid/api_key"
+}
+
+resource "aws_lambda_function" "sentimentAnalyzer" {
+  function_name = "${var.project_prefix}-sentimentAnalyzer"
+  s3_bucket     = "${var.aws_lambda_code_bucket}"
+  s3_key        = "sentimentAnalyzer.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  role          = aws_iam_role.sentimentAnalyzer_role.arn
+  timeout       = 30
+}
+
+resource "aws_lambda_function" "fetchSummary" {
+  function_name = "${var.project_prefix}-fetchSummary"
+  s3_bucket     = "${var.aws_lambda_code_bucket}"
+  s3_key        = "fetchSummary.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  role          = aws_iam_role.fetchSummary_role.name
+  timeout       = 30
+}
+
+resource "aws_lambda_function" "sendEmailNotification" {
+  function_name = "${var.project_prefix}-sendNotification"
+  s3_bucket     = "${var.aws_lambda_code_bucket}"
+  s3_key        = "sendNotification.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  role          = aws_iam_role.sendNotification_role.arn
+  timeout       = 30
+  environment {
+    variables = {
+      # Pass the secret ARN to the Lambda function as an environment variable
+      # The Lambda code will use the AWS SDK to retrieve the secret value using this ARN
+      SENDGRID_API_KEY_SECRET_ARN = data.aws_secretsmanager_secret.sendgrid_api_key_secret.arn
+      FROM_EMAIL                  = var.from_email_address # Sender email from Terraform variable
+    }
+}
+}
+
+# Lambda permission: Allow EventBridge Scheduler to invoke the Lambda
+resource "aws_lambda_permission" "allow_scheduler" {
+  statement_id  = "AllowExecutionFromScheduler"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sentimentAnalyzer.function_name
+  principal     = "scheduler.amazonaws.com"
+  source_arn    = aws_scheduler_schedule.daily_trigger.arn
+}
+
