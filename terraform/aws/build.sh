7a84115a41f7c8727fd2a977918fbb53edcfcd1d
abhisrwa <abhi1999rwa@gmail.com>
2025-05-13 08:36:19 +0400

    Update build.sh

--- terraform/aws/build.sh
+++ terraform/aws/build.sh
@@ -5,7 +5,7 @@ BUCKET_NAME="mcloud-code-bucket"
 FUNCTIONS=("sentimentAnalyzer" "fetchSummary" "sendNotification")
 
 for func in "${FUNCTIONS[@]}"; do
-  cd ../functions/$func
+  cd ../../functions/$func
   npm install --omit=dev
   zip -r ../../aws/$func.zip . -x "*.test.js"
   aws s3 cp ../../aws/$func.zip s3://$BUCKET_NAME/$func.zip
