d2da7889a75033a50a04b1b3dc1f1acda1dfdc45
abhisrwa <abhi1999rwa@gmail.com>
2025-05-13 11:08:32 +0400

    Update lambda.tf

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -8,85 +8,13 @@ data "aws_secretsmanager_secret" "sendgrid_api_key_secret" {
   name = var.aws_sendgrid_secret_name # e.g., "sendgrid/api_key"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "${var.project_prefix}-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# --- IAM Policy for Lambda Logging ---
-resource "aws_iam_policy" "lambda_logging_policy" {
-  name        = "multicloudcn-logging-policy-mc"
-  description = "IAM policy for logging from a lambda"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect = "Allow",
-        Resource = [
-            "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:logGroups:/aws/lambda/${var.aws_lambda_function_name_analysis}:*",
-            "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:logGroups:/aws/lambda/${var.aws_lambda_function_name_summary}:*",
-            "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:logGroups:/aws/lambda/${var.aws_lambda_function_name_sendmail}:*"
-        ]
-      },
-    ]
-  })
-}
-
-# --- IAM Policy to allow Lambda to read the SendGrid API Key Secret ---
-resource "aws_iam_policy" "lambda_secretsmanager_policy" {
-  name        = "${var.aws_lambda_function_name_sendmail}-secretsmanager-policy-mc"
-  description = "IAM policy for lambda to read SendGrid API Key from Secrets Manager"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret" # Sometimes needed depending on how the SDK retrieves
-        ],
-        Effect = "Allow",
-        # Grant access to the specific secret
-        Resource = data.aws_secretsmanager_secret.sendgrid_api_key_secret.arn
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_secretsmanager_attachment" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.lambda_secretsmanager_policy.arn
-}
-
-
 resource "aws_lambda_function" "sentimentAnalyzer" {
   function_name = "${var.project_prefix}-sentimentAnalyzer"
   s3_bucket     = "${var.aws_lambda_code_bucket}"
   s3_key        = "sentimentAnalyzer.zip"
   handler       = "index.handler"
   runtime       = "nodejs20.x"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = aws_iam_role.sentimentAnalyzer_role.arn
   timeout       = 30
 }
 
@@ -96,7 +24,7 @@ resource "aws_lambda_function" "fetchSummary" {
   s3_key        = "fetchSummary.zip"
   handler       = "index.handler"
   runtime       = "nodejs20.x"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = aws_iam_role.fetchSummary_role.name
   timeout       = 30
 }
 
@@ -106,7 +34,7 @@ resource "aws_lambda_function" "sendEmailNotification" {
   s3_key        = "sendNotification.zip"
   handler       = "index.handler"
   runtime       = "nodejs20.x"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = aws_iam_role.sendNotification_role.arn
   timeout       = 30
   environment {
     variables = {
@@ -118,35 +46,6 @@ resource "aws_lambda_function" "sendEmailNotification" {
 }
 }
 
-# IAM Role that EventBridge Scheduler assumes
-resource "aws_iam_role" "eventbridge_scheduler" {
-  name = "eventbridge-scheduler-lambda-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "scheduler.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-# IAM policy: Allow this role to invoke the Lambda function
-resource "aws_iam_role_policy" "eventbridge_scheduler_invoke_lambda" {
-  name = "eventbridge-scheduler-invoke-lambda"
-  role = aws_iam_role.eventbridge_scheduler.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = "lambda:InvokeFunction",
-      Resource = aws_lambda_function.sentimentAnalyzer.arn
-    }]
-  })
-}
-
 # Lambda permission: Allow EventBridge Scheduler to invoke the Lambda
 resource "aws_lambda_permission" "allow_scheduler" {
   statement_id  = "AllowExecutionFromScheduler"
@@ -156,18 +55,3 @@ resource "aws_lambda_permission" "allow_scheduler" {
   source_arn    = aws_scheduler_schedule.daily_trigger.arn
 }
 
-resource "aws_scheduler_schedule" "daily_trigger" {
-  name = "${var.project_prefix}-daily-trigger"
-
-  flexible_time_window {
-    mode = "OFF"
-  }
-
-  schedule_expression = "rate(1 hours)"
-
-  target {
-    arn      = aws_lambda_function.sentimentAnalyzer.arn
-    role_arn = aws_iam_role.eventbridge_scheduler.arn
-
-  }
-}
