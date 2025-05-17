4c078b3ea3459281caa5602dbe5bcc288293e3f8
abhisrwa <abhi1999rwa@gmail.com>
2025-05-17 22:15:27 +0400

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.azure_location
 }
 
-resource "azurerm_storage_account" "blob" {
+resource "azurerm_storage_account" "static_site" {
   name                     = "${var.project_prefix}storage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "blob" {
 
 resource "azurerm_storage_queue" "notification" {
   name                 = "js-queue-items"
-  storage_account_name = azurerm_storage_account.blob.name
+  storage_account_name = azurerm_storage_account.static_site.name
 }
 
 # Cosmos DB Account
@@ -54,7 +54,7 @@ resource "azurerm_cosmosdb_account" "cosmos" {
 
 # Cosmos DB SQL Database
 resource "azurerm_cosmosdb_sql_database" "database" {
-  name                = "appdb"
+  name                = "mccndb"
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.cosmos.name
 }
@@ -92,9 +92,106 @@ resource "azurerm_role_assignment" "fetchsummary_cosmosdb_access" {
 }
 
 resource "azurerm_role_assignment" "queue_send_permission" {
-  scope                = azurerm_storage_account.blob.id
+  scope                = azurerm_storage_account.static_site.id
   role_definition_name = "Storage Queue Data Contributor"
   principal_id         = azurerm_windows_function_app.sentimentAnalyzer.identity[0].principal_id
 }
 
+resource "azurerm_api_management" "apim" {
+  name                = "${var.project_prefix}-apim"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "admin@myapp.com"
+  publisher_email     = "admin@myapp.com"
+  sku_name            = "Consumption_0"
+}
 
+resource "azurerm_api_management_api" "summary_api" {
+  name                = "summary-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "Summary API"
+  path                = "summary"
+  protocols           = ["https"]
+
+}
+
+# Define the operation
+resource "azurerm_api_management_api_operation" "summary_post" {
+  operation_id        = "post-summary"
+  api_name            = azurerm_api_management_api.summary_api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  display_name        = "Post Summary"
+  method              = "POST"
+  url_template        = "/"
+  response {
+    status = 200
+    description = "OK"
+  }
+  request {
+    description = "Request body"
+    query_parameters = []
+    representations {
+      content_type = "application/json"
+      sample = "{\"key\": \"value\"}"
+    }
+  }
+}
+
+resource "azurerm_api_management_api_operation_policy" "summary_post_cors" {
+  api_name            = azurerm_api_management_api.summary_api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  operation_id        = "post-summary" # Must match operation ID (defined or imported)
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+    <cors allow-credentials="false">
+      <allowed-origins>
+        <origin>*</origin>
+      </allowed-origins>
+      <allowed-methods>
+        <method>POST</method>
+        <method>OPTIONS</method>
+      </allowed-methods>
+      <allowed-headers>
+        <header>*</header>
+      </allowed-headers>
+    </cors>
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
+}
+
+# Backend setting: connects APIM to the Function App endpoint
+resource "azurerm_api_management_api_operation_policy" "backend_forward" {
+  api_name            = azurerm_api_management_api.summary_api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  operation_id        = azurerm_api_management_api_operation.summary_post.operation_id
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+    <set-backend-service base-url="https://${azurerm_windows_function_app.fetchSummary.default_hostname}/api/fetchSummary" />
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
+}
