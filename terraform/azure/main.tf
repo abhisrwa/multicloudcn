6e43877cb9eb0cee72da60868d232acf9b3e5bb0
abhisrwa <abhi1999rwa@gmail.com>
2025-05-18 17:35:15 +0400

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  use_oidc = true
+  subscription_id    = var.subscription_id
+  tenant_id          = var.tenant_id
+  client_id          = var.client_id
 }
 
 resource "azurerm_resource_group" "rg" {
